0e7f1005406c850a2f0759670e451fb2646965a1
eren <drubleat@gmail.com>
2026-03-19 21:01:17 +0300

    firebase konfigürasyon yapıldı + expo sürümü downgrade edildi (simulator uyumlulugu icin)

--- SwaplyApp/src/services/firebaseConfig.js
+++ SwaplyApp/src/services/firebaseConfig.js
@@ -3,19 +3,26 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
+// firebase config bilgilerimiz
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAPtcdIyPQTfLl4GgLa6At4dWe7VIhApDo",
+  authDomain: "swaply-aa22a.firebaseapp.com",
+  projectId: "swaply-aa22a",
+  storageBucket: "swaply-aa22a.firebasestorage.app",
+  messagingSenderId: "615979080184",
+  appId: "1:615979080184:web:c7fd8b34ce5855bbbd1e0d",
+  measurementId: "G-2KS9B8BVVD"
 };
 
+
 const app = initializeApp(firebaseConfig);
+
+//persistance ayarı ile auth'un başlatılması.
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+
+// dbnin başlatılması
 const db = getFirestore(app);
 
 export { app, auth, db };
